c2a44e9255fd3189fc5714823bd2938b31866561
Roman <roman.it.kirov@yandex.ru>
2025-10-29 23:09:19 +0300

    initial commit

---
+++ Source/Minecraft/Private/Cube.cpp
@@ -0,0 +1,55 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Minecraft/Public/Cube.h"
+
+#include "Preferences/PersonaOptions.h"
+
+
+// Sets default values
+ACube::ACube()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	SetRootComponent(StaticMesh);
+	UStaticMesh* NewMesh = LoadObject<UStaticMesh>(nullptr, TEXT("/Script/Engine.StaticMesh'/Game/Cube.Cube'"));
+	if (NewMesh)
+	{
+		StaticMesh->SetStaticMesh(NewMesh);
+	}
+	StaticMesh->BodyInstance.bLockXRotation = true;
+	StaticMesh->BodyInstance.bLockYRotation = true;
+	StaticMesh->BodyInstance.bLockZRotation = true;
+	StaticMesh->BodyInstance.bLockYTranslation = true;
+	StaticMesh->BodyInstance.bLockXTranslation = true;
+	StaticMesh->bIgnoreRadialForce = true;
+	StaticMesh->bIgnoreRadialForce = true;
+	StaticMesh->SetSimulatePhysics(false);
+}
+
+// Called when the game starts or when spawned
+void ACube::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void ACube::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ACube::Fall(bool state)
+{
+	StaticMesh->SetSimulatePhysics(true);
+}
+
+void ACube::CheckFloor()
+{
+	FHitResult HitResult;
+	FVector StartLocation = GetActorLocation()*100;
+	FVector EndLocation = StartLocation + -GetActorUpVector();
+	bool bHit = GetWorld()->LineTraceSingleByChannel(HitResult,StartLocation,EndLocation,ECC_Visibility,FCollisionQueryParams());
+}
+
