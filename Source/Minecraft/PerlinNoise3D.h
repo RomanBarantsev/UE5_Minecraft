246cb505fa3818f6b3be968ee0c50e26c54edd21
Roman <roman.it.kirov@yandex.ru>
2025-11-04 21:08:39 +0300

    Perlin noice classses(inherit)

--- Source/Minecraft/PerlinNoise3D.h
+++ Source/Minecraft/PerlinNoise3D.h
@@ -1,6 +1,7 @@
 ﻿#pragma once
 
 #include "CoreMinimal.h"
+#include "PerlinNoise2D.h"
 #include "UObject/NoExportTypes.h"
 #include "PerlinNoise3D.generated.h"
 
@@ -9,16 +10,12 @@
  * Возвращает значения в прибл. диапазоне [-1, 1].
  */
 UCLASS()
-class MINECRAFT_API UPerlinNoise3D : public UObject
+class MINECRAFT_API UPerlinNoise3D : public UPerlinNoise2D
 {
 	GENERATED_BODY()
+	static float Grad(int32 Hash, float X, float Y, float Z);
 
 public:
 	UFUNCTION(BlueprintCallable, Category="Noise")
 	static float Perlin3D(float X, float Y, float Z, float Scale = 0.01f, int32 Octaves = 4, float Persistence = 0.5f, float Lacunarity = 2.0f, int32 Seed = 0);
-
-private:
-	static float Fade(float T);
-	static float Lerp(float A, float B, float T);
-	static float Grad(int32 Hash, float X, float Y, float Z);
 };
