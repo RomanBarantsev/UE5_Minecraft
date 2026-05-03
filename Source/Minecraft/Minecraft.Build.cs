220805c61740db9d8df2ff6eeca6db243b4d4c3d
Roman <roman.it.kirov@yandex.ru>
2026-05-03 19:22:24 +0300

    Update Minecraft.Build.cs
    assets registration component to get all Biomes from folder.

--- Source/Minecraft/Minecraft.Build.cs
+++ Source/Minecraft/Minecraft.Build.cs
@@ -8,7 +8,7 @@ public class Minecraft : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore","ProceduralMeshComponent", "RenderCore","GeometryCollectionEngine" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore","ProceduralMeshComponent", "RenderCore","GeometryCollectionEngine","AssetRegistry" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] { "ProceduralMeshComponent","GeometryCollectionEngine" });
 
